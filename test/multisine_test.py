126f0612871a5ff87ab1df0c7f75f26b24a193ed
Federico Scarpioni <federicoscarpioni@gmail.com>
2024-09-13 15:50:36 +0200

    Modified examples test

--- test/multisine_test.py
+++ test/multisine_test.py
@@ -2,21 +2,21 @@ from multisine.multisine import Multisine
 import numpy as np
 
 harmonics = np.loadtxt('data/harmonics_8dec_quasi-log-8pts_no_intermod_second.txt')
-base_frequency = 0.01
-max_frequency = 10000
+base_frequency = 0.1
+max_frequency = 1000
 frequencies = harmonics * base_frequency
 
 # Remove higher frequencies
 frequencies = frequencies[0:np.where(frequencies>max_frequency)[0][0]]
 frequencies[-1] = max_frequency
 
-amplitudes = np.ones(frequencies.size) * 0.025 # V
-sampling_frequency = 100000
+amplitudes = np.ones(frequencies.size) # V
+sampling_frequency = 10000
 ms1 = Multisine(sampling_frequency, frequencies, amplitudes)
+ms1.normalize_waveform()
 ms1.plot('voltage')
 ms1.fourier_analysis(6)
 ms1.plot_dft((0.01,500000))
 # ms1.plot_phase((0.1,250))
-ms1.normalize_waveform()
-ms1.best_random_phases(10)
-ms1.save('E:/multisine_collection/2408291723multisine_10kHz-10mHz_8ptd_fgen100kHz_random_phases_amplitude_from_lithium_exp_normalized')
+ms1.best_random_phases(100)
+ms1.save('E:/multisine_collection/2409131232multisine_1kHz-100mHz_8ptd_fgen10kHz_random_phases_flat_normalized')
