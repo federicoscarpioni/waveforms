2235338dae39eab1ab581bd4571d7860e7b3e6c3
Federico Scarpioni <federicoscarpioni@gmail.com>
2025-01-24 16:26:41 +0100

    Moved normalization later

--- test/multisine_test.py
+++ test/multisine_test.py
@@ -13,10 +13,10 @@ frequencies[-1] = max_frequency
 amplitudes = np.ones(frequencies.size) # V
 sampling_frequency = 10000
 ms1 = Multisine(sampling_frequency, frequencies, amplitudes)
-ms1.normalize_waveform()
 ms1.plot('voltage')
 ms1.fourier_analysis(6)
 ms1.plot_dft((0.01,500000))
 # ms1.plot_phase((0.1,250))
 ms1.best_random_phases(100)
+ms1.normalize_waveform()
 ms1.save('E:/multisine_collection/2409131232multisine_1kHz-100mHz_8ptd_fgen10kHz_random_phases_flat_normalized')
